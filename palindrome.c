eaea25c55c92c3345d8206a8724b2fb5ba9e5c59
Aman Das <92535209+UnLuCkYcOdEr69@users.noreply.github.com>
2021-10-19 10:06:49 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -9,6 +9,7 @@ int main() {
     while (n != 0) {
         remainder = n % 10;
         reversed = reversed * 10 + remainder;
+        n=n/10;
     }
 
     // palindrome if orignal and reversed are equal
