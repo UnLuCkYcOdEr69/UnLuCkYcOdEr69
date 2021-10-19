78ad028102364d127499dc9937c2449af5ba6646
Aman Das <92535209+UnLuCkYcOdEr69@users.noreply.github.com>
2021-10-19 19:17:54 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -4,6 +4,7 @@ int main()
 int value,i,factorial=1;
 printf("Enter the value:");
 scanf("%d",&value);
+    //shows error if user enter a negative number
 if (value<=0){
     printf("Error!!, Factorial of negative number doesn't exsists.");
 }
